694ed72f0cfc161ecfa45a9f85fe7430c6c793e5
PastArizox <arizoxfr@gmail.com>
2024-06-19 10:34:36 +0200

    Added javascript for skills fetching

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,33 @@
-console.log('Hello World !')
+function makeSkillElement({ url: href, icon: src, name }) {
+    return `
+    <a href="${href}" class="skill">
+        <img
+            src="${src}"
+            alt="${name ? name : 'Unknown'} logo"
+        />
+        <span>${name ? name : 'Unknown'}</span>
+    </a>
+    `;
+}
+
+function makeSkillListElement(skills) {
+    let skillElements = '';
+
+    skills.forEach((skill) => {
+        skillElements += `${makeSkillElement(skill)}\n`;
+    });
+
+    return `
+    <div class="list">
+        ${skillElements}
+    </div>
+    `;
+}
+
+fetch('assets/data/skills/tools.json')
+    .then((response) => response.json())
+    .then((tools) => {
+        const skillListElement = makeSkillListElement(tools);
+        document.querySelector('.skills .table .content').innerHTML =
+            skillListElement;
+    });
