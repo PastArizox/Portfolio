1ebdb24483217500f3ce502f508811c46f0edb35
PastArizox <arizoxfr@gmail.com>
2024-06-19 11:52:44 +0200

    Finished skills menu animation + javascript menu state

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -10,7 +10,7 @@ function makeSkillElement({ url: href, icon: src, name }) {
     `;
 }
 
-function makeSkillListElement(skills) {
+function makeSkillListElement({ skills, categoryName }) {
     let skillElements = '';
 
     skills.forEach((skill) => {
@@ -18,16 +18,59 @@ function makeSkillListElement(skills) {
     });
 
     return `
-    <div class="list">
+    <div class="list ${categoryName}">
         ${skillElements}
     </div>
     `;
 }
 
-fetch('assets/data/skills/tools.json')
+// Show/Hide Skills
+
+const skillCategories = document.querySelectorAll(
+    '.skills .table .menu .category'
+);
+
+skillCategories.forEach((category) => {
+    category.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        skillCategories.forEach((category) => {
+            category.classList.remove('active');
+        });
+
+        event.target.classList.add('active');
+
+        const categoryName = event.target.getAttribute('data-category');
+        const categoryElement = document.querySelector(
+            `.skills .table .content .${categoryName}`
+        );
+
+        document
+            .querySelectorAll('.skills .table .content .list')
+            .forEach((list) => {
+                list.style.display = 'none';
+            });
+
+        categoryElement.style.display = 'grid';
+    });
+});
+
+// Get Data
+
+fetch('assets/data/skills.json')
     .then((response) => response.json())
-    .then((tools) => {
-        const skillListElement = makeSkillListElement(tools);
-        document.querySelector('.skills .table .content').innerHTML =
-            skillListElement;
+    .then((skills) => {
+        const categoryNames = Object.keys(skills);
+
+        categoryNames.forEach((categoryName) => {
+            const skillListElement = makeSkillListElement({
+                skills: skills[categoryName],
+                categoryName,
+            });
+            document.querySelector('.skills .table .content').innerHTML +=
+                skillListElement;
+        });
+    })
+    .then(() => {
+        skillCategories[0].click();
     });
