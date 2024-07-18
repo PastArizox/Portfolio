c29b8c47ab198bde35079e8ed8af459ce7620927
PastArizox <arizoxfr@gmail.com>
2024-07-18 10:24:34 +0200

    Added header section name color on scroll + fix contact buttons size

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -174,3 +174,20 @@ function initBurgerButton() {
 }
 
 initBurgerButton();
+
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('.navbar ul li a');
+    const sections = document.querySelectorAll('section');
+
+    const changeLinkState = () => {
+        let index = sections.length;
+
+        while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
+
+        navLinks.forEach((link) => link.classList.remove('active'));
+        navLinks[index].classList.add('active');
+    };
+
+    changeLinkState();
+    window.addEventListener('scroll', changeLinkState);
+});
