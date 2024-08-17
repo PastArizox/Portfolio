01cfdfde9da6484af3d621ca759596668167c1c6
PastArizox <arizoxfr@gmail.com>
2024-08-17 13:06:48 +0200

    Updated projects section naming convention + fix sections id

--- assets/javascript/modules/project.js
+++ assets/javascript/modules/project.js
@@ -2,20 +2,22 @@ function makeProjectElement(project) {
     const projectElement = `
     <a class="project" href="${project.link}" target="_blank">
         <img
+            class="project__picture"
             src="${project.image}"
             alt="${project.name} Image"
         />
-        <div class="content">
+        <div class="project__content">
             <h3>${project.name}</h3>
             <p class="description">
                 ${project.description}
             </p>
-            <div class="footer">
-                <p>${project.date}</p>
-                <p>
+            <div class="project__footer">
+                <p class="project__date">${project.date}</p>
+                <p class="project__languages">
                 ${project.langages.map((langage) => langage).join(', ')}
                 </p>
                 <img
+                    class="project__github-logo"
                     src="assets/logo/socials/github.svg"
                     alt="Github logo"
                 />
@@ -32,7 +34,7 @@ export function loadProjects() {
         .then((response) => response.json())
         .then((projects) => {
             const projectListElement =
-                document.querySelector('#projects .list');
+                document.querySelector('.projects__list');
 
             const projectElements = projects.map((project) =>
                 makeProjectElement(project)
