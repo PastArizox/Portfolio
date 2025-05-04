cc2466d6dc821fc82944a4fb5f09851b72b5134e
PastArizox <arizoxfr@gmail.com>
2025-05-04 19:45:15 +0200

    Removed unused commented code + removed console.log

--- assets/javascript/modules/project/index.js
+++ assets/javascript/modules/project/index.js
@@ -1,15 +1,6 @@
 import { initProjects } from './logic.js';
 
 export function loadProjects() {
-    // fetch('assets/data/projects.json')
-    //     .then((response) => response.json())
-    //     .then((projects) => {
-    //         const projectListElement =
-    //             document.querySelector('.projects__list');
-
-    //         initProjects(projectListElement, projects);
-    //     });
-
     fetch(
         'https://api.github.com/users/PastArizox/repos?sort=created&direction=desc'
     )
@@ -19,8 +10,6 @@ export function loadProjects() {
                 document.querySelector('.projects__list');
 
             initProjects(projectListElement, projects);
-
-            console.log(projects[0]);
         })
         .catch((error) => {
             console.error('Error fetching GitHub projects:', error);
